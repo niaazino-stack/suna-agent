fe76d1992a940d42bcc343026af93857a80a0688
arian.rad7878 <arian.rad7878@gmail.com>
2025-11-21 22:49:18 +0000

    Changes before Firebase Studio auto-run

--- apps/mobile/components/auth/index.ts
+++
@@ -1,6 +0,0 @@
-export { GuestAuthGate } from './GuestAuthGate';
-export { GuestModeConsent } from './GuestModeConsent';
-export { AuthDrawer } from './AuthDrawer';
-export { SignInForm, SignUpForm } from './AuthForms';
-export * from './types';
-
